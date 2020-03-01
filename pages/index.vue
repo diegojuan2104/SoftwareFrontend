5e421c3b87ddc33b36b6c2ec91053c064ecf36e5
Diego <diegojuan2104@gmail.com>
2020-03-01 18:53:33 -0500

    Pequeña prueba de bootstrap

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,19 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Propuestas
-      </h1>
-      <h2 class="subtitle">
-        Este es un proyecto para la gentión de propuestas de convenios entre universidades de la Universidad de Medellin
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template >
+<div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+
 </template>
+
+
 
 <script>
 import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
+  
+  components:{
+
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
