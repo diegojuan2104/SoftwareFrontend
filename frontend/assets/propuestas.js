807a37d662de1f745cd1f81f38f1ac520709dcf9
Juan Diego Mejía Otálvaro <diegojuan2104@hotmail.com>
2020-05-05 00:26:57 -0500

    Detalles

--- frontend/assets/propuestas.js
+++ frontend/assets/propuestas.js
@@ -32,26 +32,29 @@ export default {
   methods: {
     async cargarPropuestas() {
       try {
+        this.propuestaReducida = [];
         this.propuestas = [];
-        this.propuestasReducidas = [];
         let url = config.url_api;
         const res = await axios.get("http://localhost:3001/api/v1/propuestas");
         this.propuestas = res.data;
+        console.log(this.propuestas);
 
         //Se añaden datos a lista reducida
-        this.propuestas.forEach(propuesta => {
+
+        for (let i = 0; i < this.propuestas.length; i++) {
           let propuestaReducida = {
-            id_Propuesta: propuesta.idpropuesta,
-            nombreEntidad: propuesta.nombreentidad,
-            nombre: propuesta.nombrecompletopersona,
-            email: propuesta.email,
-            estadoPropuesta: propuesta.estadoconvenio,
+            id_Propuesta: this.propuestas[i].idpropuesta,
+            nombreEntidad: this.propuestas[i].nombreentidad,
+            nombre: this.propuestas[i].nombrecompletopersona,
+            email: this.propuestas[i].email,
+            estadoPropuesta: this.propuestas[i].estadoconvenio,
             Modificar: true,
             Eliminar: true
           };
           this.propuestasReducidas.push(propuestaReducida);
           console.log(this.propuestasReducidas);
-        });
+        }
+
         console.log(this.propuestas);
       } catch (error) {
         console.log(error);
