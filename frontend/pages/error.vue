da3fa343dc25f762e20ac7f789341ac6699063f2
JuanJose202 <juanjosegomez1908@gmail.com>
2020-05-18 20:37:13 -0500

    pagina de error lista

--- frontend/pages/error.vue
+++ frontend/pages/error.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <h3>Sesión terminada o inaccesible</h3>
-  </div>
+<b-container>
+           <b-img
+          src="../images/403.jpg"
+          fluid
+          alt="Fluid image"
+        ></b-img>
+        <div class="text">Lo sentimos, usted no tiene permisos para acceder a este módulo</div>
+
+</b-container>
 </template>
+<style scoped>
 
+.text {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+    align-content: stretch;
+    color: #ED0034;
+    font-size: 25px;
+}
 
-<script>
-export default {
-  layout: "error"
-};
-</script>
+</style>
